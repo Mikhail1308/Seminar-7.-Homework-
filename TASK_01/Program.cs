b963b3ffb642ba74d23eccccc7d9b048d2361483
Mikhail Nikolaev <armavir1308@mail.ru>
2023-02-06 20:54:57 +0300

    TASK_01. Создание массива с вещественными числами

---
+++ TASK_01/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreatArray(int m = 3, int n = 4)
+{
+    Random rnd = new Random();
+    double[,] array = new double[3, 4];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round((rnd.NextDouble() * (1 - 20) + 10), 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+double[,] resultArray = CreatArray();
+PrintArray(resultArray);
+Console.WriteLine();
